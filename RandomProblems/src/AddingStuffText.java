fb13b96c353de70fe91dfe174e45dd29d46063b6
Eerian <erionpali@erions-mbp>
2014-11-01 23:26:06 -0400

    Second Sync 10/1/14

---
+++ RandomProblems/src/AddingStuffText.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class AddingStuffText {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Adding more shit
+		int test = 2;
+		
+		System.out.println("Tesind adding stuff to git");
+	}
+
+}
