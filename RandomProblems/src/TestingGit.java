334898764d22c7800c18f5ac6f3bc91fa2700dd9
Eerian <erionpali@erions-mbp>
2014-11-01 23:15:10 -0400

    11/1/14

---
+++ RandomProblems/src/TestingGit.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class TestingGit {
+
+	public static void main(String[] args) {
+		// Testing
+		Scanner input = new Scanner(System.in);
+		int n = 29;
+		int n2 = 4;
+		int realAge = n - n2;
+		String name = "Erick";
+		System.out.println(" What's your favorite car? ");
+		int car = input.nextInt();
+		
+		
+	System.out.println(" Erick " + " is " + realAge + " years old.");
+		
+
+	}
+
+}
